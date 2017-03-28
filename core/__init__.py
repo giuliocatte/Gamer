3f7aef5c1a2ab7732fcfd71e43bf2cae41a46a7c
giuliocatte <giuliocatellani@iMac-di-Giulio-2.lan>
2017-03-28 23:55:53 +0200

    updated todo list

--- core/__init__.py
+++ core/__init__.py
@@ -1,15 +1,16 @@
 '''
     TODO LIST:
 
-    - an utility to put an AI versus another, run a thousand of games and give winning stats
     - reinforcement learning
     - alpha beta pruning for minimax
     - an AI that combines minimax and neural network (using nn only when the outcome is not clear)
+    - understanding why appling the nn player is so slow (i'd have guessed "training slow but apply fast")
     - when minimax detects a loss ahead, it should play hoping for an opponent error
     - asyncio for running the players simultaneously (in simultaneous games)
     - control of timeouts for AIs
-    - chess and go
+    - chess, go, checkers, backgammon and so on
     - a transformation of a trained neural network in a python function, to copypaste it where I want to use it, without
      the need of tensorflow
+    - a GUI
 
 '''
